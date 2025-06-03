2e1526e38c04a3f6819487758475c53dca974ee8
randers444 <ryan.anders444@gmail.com>
2025-06-03 00:03:46 -0500

    add deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://theropods.github.io',
+    base: 'run-types',
   vite: {
     plugins: [tailwindcss()]
   }
